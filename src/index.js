98df0e46d5d8cd5d68c5cd7f5ebb2268756135ea
ican <xyxcnnh@gmail.com>
2018-07-06 22:07:50 +0800

    前端引入redux相关依赖

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
